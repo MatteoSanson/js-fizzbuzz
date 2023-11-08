b46f056129f56bdb1928e9c6c0f07db9e6418bef
MatteoSanson <matteo.sanson@gmail.com>
2023-11-08 17:47:54 +0100

    feat: bonus1

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i mult
 - se è multipo di 3 stampo Fizz.
 - altrimenti stampo la mia variabile ciclo.
 
+
+## BONUS 1
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+### Svolgimento Bonus1
+1) aggiungo un elemento \<ul> all'interno del file index.html e una variabile in js.
+2) creo in js l'elemento \<li> con le sue variabili e cosa deve contenere.
+3) nelle condizioni stabilite dall'esercizio precedente dico cosa "stampare" in html.
+4) \<li> e il loro contenuto fanno parte del codice html.
--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 'use strict';
+// parte 1 bonus1 
+let elementUl = document.querySelector('ul.list');
 
 // variabili per ciclo 
 let contatoreInizio = 1;
 let contatoreFine = 100;
+
 // 1) Definisco un ciclo per un conteggio incrementato da 1 a 100 e stampo ogni numero in console.
 for (let i=contatoreInizio; i<= contatoreFine; i++){
+    // bonus1 2) 
+    let elementLi = document.createElement('li');
+    let listText = '';
 
     // 2) Stabilisco delle condizioni che: 
 
     // se è multipo di 3 e di 5 stampo FizzBuzz.
     if (i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz');
+        // bonus1 3) 
+        listText = 'FizzBuzz';
     // se è multiplo di 5 stampo Buzz. 
     } else if (i % 5 === 0){
         console.log('Buzz');
+        // bonus1 3) 
+        listText = 'Buzz';
     // se è multiplo di 3 stampo Fizz.
     } else if (i % 3 === 0){
         console.log('Fizz');
+        // bonus1 3) 
+        listText = 'Fizz';
     // altrimenti stampo la mia variabile ciclo. 
     } else {
         console.log(i);
+        // bonus1 3) 
+        listText = i;
     }
-    
+
+    // bonu1 4) 
+    elementLi.textContent = listText;
+    elementUl.append(elementLi);
 }
 
