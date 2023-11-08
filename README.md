dccc96356a5d162b41b355ee8f847e75ed505402
MatteoSanson <matteo.sanson@gmail.com>
2023-11-08 16:16:41 +0100

    fix: update condizioni

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i mult
 ## Svolgimento
 1) Definisco un ciclo per un conteggio incrementato da 1 a 100 e stampo ogni numero in console.
 2) Stabilisco delle condizioni che:
-- se è multiplo di 3 stampo Fizz.
+- se è multiplo di 3 e 5 stampo FizzBuzz.
 - se è multiplo di 5 stampo Buzz.
-- se è multipo di 3 e di 5 stampo FizzBuzz.
+- se è multipo di 3 stampo Fizz.
 - altrimenti stampo la mia variabile ciclo.
--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log("sto funzionando")
+// variabili per ciclo 
+let contatoreInizio = 1;
+let contatoreFine = 100;
+// 1) Definisco un ciclo per un conteggio incrementato da 1 a 100 e stampo ogni numero in console.
+for (let i=contatoreInizio; i<= contatoreFine; i++){
+
+    // 2) Stabilisco delle condizioni che: 
+
+    // se è multipo di 3 e di 5 stampo FizzBuzz.
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
+    // se è multiplo di 5 stampo Buzz. 
+    } else if (i % 5 === 0){
+        console.log('Buzz');
+    // se è multiplo di 3 stampo Fizz.
+    } else if (i % 3 === 0){
+        console.log('Fizz');
+    // altrimenti stampo la mia variabile ciclo. 
+    } else {
+        console.log(i);
+    }
+    
+}
+
